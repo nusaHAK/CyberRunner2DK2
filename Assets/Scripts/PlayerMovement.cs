946b74ffdd6e96c63cf8cd2016b683157307713c
nusaHAK <36762707+nusaHAK@users.noreply.github.com>
2026-03-27 07:21:35 +0100

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb; // Neue Variable für Physik
+
+    public TextMeshProUGUI scoreText;
+    private int score = 0;  
+
+    void Start() {
+        // Wir holen uns die Verbindung zum Rigidbody
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float moveX = Input.GetAxis("Horizontal");
+        
+        // Alte Zeile mit Translate LÖSCHEN!
+        // Neue Physik-Bewegung: Wir setzen die Geschwindigkeit direkt
+        // Wir behalten die aktuelle y-Geschwindigkeit (fallen/springen) bei
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+
+        // Springen mit Leertaste
+        if (Input.GetButtonDown("Jump")) {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // Wenn das andere Objekt den Tag "Coin" hat...
+        if (other.CompareTag("Coin")) {
+            score++; // Zähler hoch
+            scoreText.text = "Coins: " + score; // Text ändern
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
